a3496db486defd7ee79636d41ee2e1233dc4674a
Foshati <amirrezafoshati@gmail.com>
2024-05-20 23:13:44 +0330

    start project 2 react 19 beta learn and test

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { version } from "react";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-red-500 text-3xl m-4">React Version: {version}</h1>
+    </>
+  );
+}
+
+export default App;
